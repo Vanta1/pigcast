aab9713e1c077f2aa0c4dc1422c8dfa8cfaac76e
Vanta_1 <mcoopersandys@gmail.com>
2024-12-22 15:29:27 -0500

    letsa go

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,31 @@
 #![allow(non_snake_case)]
 
 use dioxus::{
-    desktop::{use_window, Config, WindowBuilder},
+    desktop::{Config, WindowBuilder},
     prelude::*,
 };
-use document::Stylesheet;
-
-static CSS: Asset = asset!("/assets/main.css");
 
 fn main() {
     let window = WindowBuilder::new()
         .with_title("Pigcast")
         .with_decorations(false);
-    let config = Config::new().with_window(window);
+
+    let style = include_str!("../assets/style.css");
+    let head = format!(
+        r#"
+            <title>Pigcast</title>
+            <style>{style}</style>
+        "#
+    );
+
+    let config = Config::new().with_window(window).with_custom_head(head);
+
     dioxus::LaunchBuilder::new().with_cfg(config).launch(App);
 }
 
 #[component]
 fn App() -> Element {
-    use_window().set_decorations(false);
     rsx! {
-        Stylesheet { href: CSS }
         p { "Hello porld (porb world)" }
     }
 }
