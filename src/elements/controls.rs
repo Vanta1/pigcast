ae449c8cf2803e7beeca3eb29152fdb53df97fe6
Vanta_1 <mcoopersandys@gmail.com>
2025-03-25 02:54:09 -0400

    icons working ig

--- src/elements/controls.rs
+++ src/elements/controls.rs
@@ -13,13 +13,37 @@ impl Render for PigControls {
         _window: &mut gpui::Window,
         _cx: &mut gpui::Context<'_, Self>,
     ) -> impl IntoElement {
+        let icon_size = rems(2.);
+        let icon_color = self.theme.gray();
+
         div()
-            .h(rems(3.))
+            .h(rems(4.))
             .w(DefiniteLength::Fraction(1.))
             .flex()
+            .flex_row()
             .justify_center()
-            .border_t_1()
+            .items_center()
+            .border_t(rems(0.2))
             .border_color(self.theme.bg1())
-            .child(svg().path("icons/play.svg").size_10())
+            .child(
+                svg()
+                    .path("icons/backward.svg")
+                    .size(icon_size - rems(0.0))
+                    .text_color(icon_color),
+            )
+            .child(
+                svg()
+                    .ml(rems(0.6))
+                    .mr(rems(0.5))
+                    .path("icons/play.svg")
+                    .size(icon_size + rems(0.0))
+                    .text_color(icon_color),
+            )
+            .child(
+                svg()
+                    .path("icons/forward.svg")
+                    .size(icon_size - rems(0.0))
+                    .text_color(icon_color),
+            )
     }
 }
